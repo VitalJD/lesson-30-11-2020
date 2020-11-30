cf6c608196f07f8d193c2883777a4177493ef784
Vitaly <vitalyborisenko@gmail.com>
2020-11-30 14:35:41 +0300

    123

---
+++ src/app/modul1/modul1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Modul1Component } from './modul1.component';
+
+
+
+@NgModule({
+  declarations: [Modul1Component],
+  imports: [
+    CommonModule
+  ]
+})
+export class Modul1Module { }
---
+++ src/app/modul1/modul2/modul2.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Modul2Component } from './modul2.component';
+
+describe('Modul2Component', () => {
+  let component: Modul2Component;
+  let fixture: ComponentFixture<Modul2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Modul2Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Modul2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
